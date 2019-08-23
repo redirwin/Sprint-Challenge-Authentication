587dd5247f391be47505094383c92a26de80b125
Dave Irwin <david@redirwin.com>
2019-08-23 09:41:36 -0700

    Sucessfully creating token at login.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -37,7 +37,7 @@ router.post("/login", (req, res) => {
       .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
           const token = createToken(user);
-          res.status(200).json({ message: `Welcome, ${user.username}` });
+          res.status(200).json({ message: `Welcome, ${user.username}`, token });
         } else {
           res.status(401).json({ message: "Those credentials aren't valid." });
         }
