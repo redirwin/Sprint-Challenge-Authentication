ba34bab24ca91efc003d3791a728cd1e0f34c8e9
Dave Irwin <david@redirwin.com>
2019-08-23 09:03:33 -0700

    Checking for username and password.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,17 +5,22 @@ const Users = require("../users/users-model.js");
 
 router.post("/register", (req, res) => {
   // implement registration
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 12); // hash the password
-  user.password = hash; // reset password as hashed password
 
-  Users.addUser(user)
-    .then(newUser => {
-      res.status(201).json(newUser);
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
+  if (req.body.username && req.body.password) {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 12); // hash the password
+    user.password = hash; // reset password as hashed password
+
+    Users.addUser(user)
+      .then(newUser => {
+        res.status(201).json(newUser);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  } else {
+    res.status(400).json({ message: "Please enter a username and password." });
+  }
 });
 
 router.post("/login", (req, res) => {
